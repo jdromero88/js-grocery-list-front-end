99984e28be2da218b1057ac75db5cf34b2f92ff2
jdromero88 <jdromerov88@gmail.com>
2021-03-17 08:59:15 -0400

    DRYing code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,14 @@
 let productos = []
 let year = new Date()
+
+// Crear elementos
+function crearElemento(elemento) {
+	return document.createElement(elemento)
+}
+
 function iniciaTodo() {
 	console.log('connected')
 	let footerEl = document.getElementById('footer')
-
-	// TODO: agregar texto en el footer
 	footerEl.innerHtml = year.getFullYear() + `&copy; Grocery List`
 }
 
@@ -17,21 +21,21 @@ function validateInput(producto, cantidad) {
 }
 
 function addProducto() {
+	let productoLiEl = crearElemento('li')
+
 	let listaOlEl = document.getElementById('lista')
-	let productoLiEl = document.createElement('li')
 	let productoParaAgregar = document.getElementById('nuevoProducto')
 	let cantidad = document.getElementById('cantidad')
+
 	if (!validateInput(productoParaAgregar.value, cantidad.value)) {
 		alert('Debe agregar un producto')
 		productoParaAgregar.focus()
 		return
 	}
+
 	let p = new Producto(productoParaAgregar.value, cantidad.value)
-	console.log(p.name)
-	console.log(p.cantidad)
 	productos.push(p)
 	productoLiEl.innerHTML = p.name + " <strong>quantity</strong>: " + p.cantidad
-	// productoLiEl.innerText = p.name + " quantity: " + p.cantidad
 
 	listaOlEl.appendChild(productoLiEl)
 }
